f0506209ec3494a52055d401882c566d368d3095
Abul Hasan <abulhasan.swe@gmail.com>
2022-03-17 21:45:11 -0700

    Added Checkout Functionality

--- frontend/src/components/checkout/Checkout.js
+++ frontend/src/components/checkout/Checkout.js
@@ -1,21 +1,53 @@
-import React, { useEffect } from 'react'
-import { getToken } from "../../redux/selectors";
+import React, { useState,useEffect } from "react";
+import { getTokenAndCart } from "../../redux/selectors";
+import { addCart } from "../../redux/cartSlice";
 import { connect } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import './checkout.css';
+import CartItem from "../cartitem/CartItem";
+import "./checkout.css";
 
 function Checkout(props) {
 
+  const totalPrice = useState(0);
+  let tempPrice = 0;
+  let totalItems = 0;
+  let totalQuantity = 0;
   const navigate = useNavigate();
+  console.log(props);
 
-  useEffect(()=>{
-    if(!props.token){
+  useEffect(() => {
+    if (!props.token) {
       navigate("../login", { replace: true });
     }
-  },[])
+  }, []);
+
+  const purchaseItems = () =>{
+    
+  }
   return (
-    <div>Checkout</div>
-  )
+    <div className="checkout__container">
+      <div className="checkout__itemcontainer">
+        <div className="checkout__header">Shopping Cart</div>
+      {Object.values(props.cart).map((item) => {
+        tempPrice+=item.price*item.quantity;
+        totalItems++;
+        totalQuantity+=item.quantity
+        console.log("temprice",tempPrice);
+        return <CartItem
+          // key={favourites[item.item_id] || item.item_id}
+          item={item}
+      />
+      })}
+      </div>
+      <div className="checkout__order">
+      <div className="checkout__header2"> Subtotal</div>
+      <div className="checkout__totalitems"><span>No of Items:</span><span>{totalItems}</span></div> 
+      <div className="checkout__totalitems"><span>Total Quantity:</span><span>{totalQuantity}</span></div> 
+       <div className="checkout__totalprice"><span>Total Price:</span> <span>${tempPrice}</span></div>
+        <input type="button" value='Purchase Items' className="checkout__btn"/>
+      </div>
+    </div>
+  );
 }
 
-export default connect(getToken,null)(Checkout);
+export default connect(getTokenAndCart, { addCart })(Checkout);
